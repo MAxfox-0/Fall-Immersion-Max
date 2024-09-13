4a8612b21608d8a8b88c00c860ebb420a8378747
MAxfox-0 <maxwellesayas@gmail.com>
2024-09-12 17:03:32 -0700

    #1

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,6 @@
 function counter() {
 // Write your code below. Hint: you can finish it in about 3-5 lines.
-}
+    counter_Number = document.getElementById('counter-number');
+    // Adds 1 each time counter_Number is clicked
+    counter_Number.innerHTML = parseInt(counter_Number.innerHTML) + 1;
+    }

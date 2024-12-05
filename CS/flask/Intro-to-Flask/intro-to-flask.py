6a36ab919eeef613dc3fa48e478cb42b841ddb9a
MAxfox-0 <maxwellesayas@gmail.com>
2024-12-04 17:41:33 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+if __name__ == '__main__':
+    app.run(debug = True)
 # instruction will below. 
